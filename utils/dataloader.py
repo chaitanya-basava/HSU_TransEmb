23b9a4945a2500b02c03e9f3bdbed45386850bfb
chaitnayabasava <viswachaitanya.b16@iiits.in>
2021-05-11 21:27:01 +0530

    bug fix.

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -32,7 +32,63 @@ def get_dataloader_task1(
     # DATA proportion
     grp = train.groupby(["category"])["id"].nunique()
     train_prop = {key: grp[key] for key in list(grp.keys())}
-    class_wts = np.array([1./train_prop[c] for c in ["NOT", "OFF"]])
+    class_wts = np.array([1.0 / train_prop[c] for c in ["NOT", "OFF"]])
+
+    # class_wts = {c: train_prop[c] / len(train) for c in ["NOT", "OFF"]}
+    # example_wts = [1./class_wts[c] for c in train.category]
+    # sampler = WeightedRandomSampler(example_wts, len(train))
+
+    train_dataset = Task1Dataset(
+        train,
+        tokenizer=tokenizer,
+        max_len=max_length,
+        padding_type=padding_type,
+    )
+
+    val_dataset = Task1Dataset(
+        val,
+        tokenizer=tokenizer,
+        max_len=max_length,
+        padding_type=padding_type,
+    )
+
+    return (
+        DataLoader(
+            dataset=train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+        ),
+        DataLoader(
+            dataset=val_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+        ),
+        torch.tensor(class_wts, dtype=torch.float),
+    )
+
+
+def get_dataloader_task2(
+    data_path,
+    file_name,
+    model,
+    batch_size=64,
+    max_length=256,
+    padding_type="max_length",
+):
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    train_path = os.path.join(data_path, file_name + "train.tsv")
+    train = pd.read_csv(train_path, sep="\t")
+
+    val_path = os.path.join(data_path, file_name + "dev.tsv")
+    val = pd.read_csv(val_path, sep="\t")
+
+    # DATA proportion
+    grp = train.groupby(["category"])["id"].nunique()
+    train_prop = {key: grp[key] for key in list(grp.keys())}
+    class_wts = np.array([1.0 / train_prop[c] for c in ["NOT", "OFF"]])
+
+    print(class_wts)
 
     # class_wts = {c: train_prop[c] / len(train) for c in ["NOT", "OFF"]}
     # example_wts = [1./class_wts[c] for c in train.category]
