bae772453f685caf5cdf0766629ef863c85ff9ae
chaitnayabasava <viswachaitanya.b16@iiits.in>
2021-06-10 21:22:50 +0530

    store representations.

--- inference.py
+++ inference.py
@@ -16,38 +16,45 @@ from transformer.model import TransformerClassifier
 with open("./config.yaml") as file:
     config = yaml.safe_load(file)
 
-# if not config["model"]["ckpt"]:
-#     print("Checkpoint is needed!!!")
-#     sys.exit(0)
+if not config["model"]["ckpt"]:
+    print("Checkpoint is needed!!!")
+    sys.exit(0)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# model_dir = os.path.join(
-#     config["model"]["model_loc"],
-#     config["dataset"]["file_name"],
-#     config["model"]["model"].split("/")[-1],
-# )
+model_dir = os.path.join(
+    config["model"]["model_loc"],
+    config["dataset"]["file_name"],
+    config["model"]["model"].split("/")[-1],
+)
 
-# _model = TransformerClassifier(
-#     config["model"]["model"],
-#     hidden_states=config["hyperparameters"]["hidden_layers"],
-#     dropout=config["hyperparameters"]["dropout"],
-# ).to(device)
+if config["inference"]["save_representations"]:
+    rep_dir = os.path.join(model_dir, "representations")
+    if not os.path.exists(rep_dir):
+        os.makedirs(rep_dir)
 
-# _model, _, _, best_weighted_f1, _ = load_checkpoint(
-#     config["model"]["ckpt"],
-#     config["model"]["model"],
-#     model_dir,
-#     device,
-#     _model,
-#     None,
-#     None,
-# )
+_model = TransformerClassifier(
+    config["model"]["model"],
+    hidden_states=config["hyperparameters"]["hidden_layers"],
+    dropout=config["hyperparameters"]["dropout"],
+).to(device)
 
-# criterion = nn.CrossEntropyLoss()
+print(config["model"]["ckpt"])
 
-# print(f"Model with wted F1 - {best_weighted_f1} is loaded!!!")
+_model, _, _, best_weighted_f1, _ = load_checkpoint(
+    config["model"]["ckpt"],
+    config["model"]["model"],
+    model_dir,
+    device,
+    _model,
+    None,
+    None,
+)
+
+criterion = nn.CrossEntropyLoss()
+
+print(f"Model with wted F1 = {best_weighted_f1} is loaded!!!")
 
 test_loader = get_testloader(
     config["dataset"]["data_dir"],
@@ -59,41 +66,40 @@ test_loader = get_testloader(
 )
 
 test_loss, test_acc = [], []
-rep_data = {}
 
-# _model.eval()
+_model.eval()
 y_preds, y_test = np.array([]), np.array([])
 with torch.set_grad_enabled(False):
     with tqdm(test_loader, desc="") as vepoch:
+        vepoch.set_postfix(loss=0.0, acc=0.0)
         for batch_idx, batch in enumerate(vepoch):
-            print(batch)
-            sys.exit(0)
-            # details, ypred, ytrue, representations = step(_model, batch, criterion, device)
+            details, ypred, ytrue, representations = step(
+                _model, batch, criterion, device
+            )
 
-            # if config["inference"]["save_representations"]:
-            #     text = batch["text"]
-            #     representations = representations.cpu().numpy()
+            if config["inference"]["save_representations"]:
+                ids = batch["id"]
+                representations = representations.cpu().numpy()
 
-            #     for i, j in zip(batch, representations):
-            #         rep_data[i["text"]] = representations
+                for i, j in zip(ids, representations):
+                    np.save(f"{rep_dir}/{i}.npy", j)
 
-            # y_preds = np.hstack((y_preds, ypred.cpu().numpy()))
-            # y_test = np.hstack((y_test, ytrue.to("cpu").numpy()))
+            y_preds = np.hstack((y_preds, ypred.cpu().numpy()))
+            y_test = np.hstack((y_test, ytrue.to("cpu").numpy()))
 
-            # test_loss.append(details["loss"].item())
-            # test_acc.append(details["accuracy"].item())
+            test_loss.append(details["loss"].item())
+            test_acc.append(details["accuracy"].item())
 
-            # vepoch.set_postfix(
-            #     loss=details["loss"].item(), acc=np.array(test_acc).mean()
-            # )
+            vepoch.set_postfix(
+                loss=details["loss"].item(), acc=np.array(test_acc).mean()
+            )
 
-# print(
-#     classification_report(
-#         y_test,
-#         y_preds,
-#         target_names=["OFF", "NOT"],
-#     )
-# )
+print(
+    classification_report(
+        y_test,
+        y_preds,
+        target_names=["OFF", "NOT"],
+    )
+)
 
-# if config["inference"]["save_representations"]:
-#     np.save(f"{model_dir}/representations.npy", rep_data)
+print(f"{config['inference']['mode']} data processed")
--- transformer/model.py
+++ transformer/model.py
@@ -7,14 +7,14 @@ class TransformerClassifier(nn.Module):
     def __init__(self, model_name, hidden_states=256, dropout=0.5, n_classes=2):
         super().__init__()
 
-        self.transmodel = AutoModel.from_pretrained(model_name)
-        self.lin0 = nn.Linear(self.transmodel.config.hidden_size, hidden_states)
+        self.bert = AutoModel.from_pretrained(model_name)
+        self.lin0 = nn.Linear(self.bert.config.hidden_size, hidden_states)
         self.lin1 = nn.Linear(hidden_states, n_classes)
         self.reLU = nn.ReLU()
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, input_ids, attention_mask):
-        output = self.transmodel(input_ids=input_ids, attention_mask=attention_mask)
+        output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         output = output.last_hidden_state
         output = torch.mean(output, dim=1)
         y = self.lin0(output)
